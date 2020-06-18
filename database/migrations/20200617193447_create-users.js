9b74d40f0db27ac8ea0e35169a978c002f1dcc6f
CyrusTheCurator <joshua.c.luscombe@gmail.com>
2020-06-17 21:12:21 -0700

    misc fix

--- database/migrations/20200617193447_create-users.js
+++ database/migrations/20200617193447_create-users.js
@@ -1,12 +1,24 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("users", (users) => {
-    users.increments();
-
-    users.string("username", 255).notNullable().unique();
-    users.string("password", 255).notNullable();
-  });
+  return knex.schema
+    .createTable("roles", (tbl) => {
+      tbl.increments();
+      tbl.string("name", 128).notNullable().unique();
+    })
+    .createTable("users", (tbl) => {
+      tbl.increments();
+      tbl.string("username", 128).notNullable().unique().index();
+      tbl.string("email", 256).notNullable().unique();
+      tbl.string("password", 256).notNullable();
+      tbl
+        .integer("role")
+        .unsigned()
+        .references("roles.id")
+        .defaultTo(1)
+        .onDelete("RESTRICT")
+        .onUpdate("CASCADE");
+    });
 };
 
 exports.down = function (knex, Promise) {
-  return knex.schema.dropTableIfExists("users");
+  return knex.schema.dropTableIfExists("users").dropTableIfExists("roles");
 };
