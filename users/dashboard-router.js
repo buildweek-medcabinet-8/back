3a0d39b6a1d8a4b5ef51b182fade421863722e90
CyrusTheCurator <joshua.c.luscombe@gmail.com>
2020-06-18 19:50:09 -0700

    clarify message on delete

--- users/dashboard-router.js
+++ users/dashboard-router.js
@@ -3,6 +3,7 @@ const Users = require("./users-model");
 
 router.get("/", (req, res) => {
   const [directive, token] = req.headers.authorization.split(" ");
+  let user = req.decodedJwt.username;
 
   res.status(200).json({
     message: `welcome to your secret page, ${user}`,
@@ -14,10 +15,8 @@ router.get("/delete-user", (req, res) => {
   let user = req.decodedJwt.username;
   Users.remove(user)
     .then((rmvdUsr) => {
-      req.decodedJwt = {};
       res.status(200).json({
-        message: `YOU JUST DELETED,`,
-        usr: rmvdUsr,
+        message: `YOU JUST DELETED, ${user}, be sure to delete the token from memory`,
       });
     })
     .catch((err) => {
