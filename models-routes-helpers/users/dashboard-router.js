dd0561780c25255a2fe7dd96e5e5cebc863d3e05
CyrusTheCurator <joshua.c.luscombe@gmail.com>
2020-06-26 17:49:38 -0700

    refactor list code

--- models-routes-helpers/users/dashboard-router.js
+++ models-routes-helpers/users/dashboard-router.js
@@ -6,6 +6,88 @@ const axios = require("axios");
 const FormData = require("form-data");
 //router.use("/list", listsRouter);
 
+function turnIDsIntoInfo(effects, flavors, descriptions) {
+  let resObj = {};
+  console.log("start effects ", effects, " end effects");
+  console.log("start flavors ", flavors, " end flavors");
+  effects.forEach((effect) => {
+    if (!resObj[effect.listName]) {
+      resObj[effect.listName] = {
+        effects: [],
+      };
+    }
+    resObj = {
+      ...resObj,
+      [effect.listName]: {
+        ...resObj[effect.listName],
+        effects: [...resObj[effect.listName].effects, effect.effect],
+      },
+    };
+  });
+
+  flavors.forEach((flavor) => {
+    if (resObj[flavor.listName].flavors === undefined) {
+      resObj[flavor.listName] = {
+        effects: [...resObj[flavor.listName].effects],
+        flavors: [],
+      };
+    }
+
+    resObj = {
+      ...resObj,
+      [flavor.listName]: {
+        ...resObj[flavor.listName],
+        flavors: [...resObj[flavor.listName].flavors, flavor.flavor],
+      },
+    };
+  });
+
+  descriptions.forEach((description) => {
+    if (resObj[description.listName].descriptions === undefined) {
+      resObj[description.listName] = {
+        effects: [...resObj[description.listName].effects],
+        flavors: resObj[description.listName].flavors,
+
+        description: [],
+      };
+    }
+    console.log(description, " this shouldn't be undefined");
+    resObj = {
+      ...resObj,
+      [description.listName]: {
+        ...resObj[description.listName],
+        description: [
+          ...resObj[description.listName].description,
+          description.userDescription,
+        ],
+      },
+    };
+  });
+  console.log("the res obj coign back isssss ", resObj);
+  return resObj;
+}
+
+async function makePrefstringFromList(id, listName) {
+  let prefs = [];
+
+  let effects = await Users.getList(id, "effects", listName);
+  let flavors = await Users.getList(id, "flavors", listName);
+  let descriptions = await Users.getList(id, "list_descriptions", listName);
+  let resObj = turnIDsIntoInfo(effects, flavors, descriptions);
+  resObj = resObj[Object.keys(resObj)[0]];
+
+  resObj.effects.map((effect) => {
+    prefs.push(effect);
+  });
+  resObj.flavors.map((flavors) => {
+    prefs.push(flavors);
+  });
+  prefs.push(resObj.description[0]);
+  let prefsString = prefs.join(" ");
+
+  return prefsString;
+}
+
 async function getRecs(prefs) {
   try {
     const formData = new FormData();
@@ -50,62 +132,8 @@ router.get("/lists", async (req, res) => {
     let effects = await Users.getLists(id, "effects");
     let flavors = await Users.getLists(id, "flavors");
     let descriptions = await Users.getLists(id, "list_descriptions");
-    let resObj = {};
-    console.log("start effects ", effects, " end effects");
-    console.log("start flavors ", flavors, " end flavors");
-    effects.forEach((effect) => {
-      if (!resObj[effect.listName]) {
-        resObj[effect.listName] = {
-          effects: [],
-        };
-      }
-      resObj = {
-        ...resObj,
-        [effect.listName]: {
-          ...resObj[effect.listName],
-          effects: [...resObj[effect.listName].effects, effect.effect],
-        },
-      };
-    });
-
-    flavors.forEach((flavor) => {
-      if (resObj[flavor.listName].flavors === undefined) {
-        resObj[flavor.listName] = {
-          effects: [...resObj[flavor.listName].effects],
-          flavors: [],
-        };
-      }
-
-      resObj = {
-        ...resObj,
-        [flavor.listName]: {
-          ...resObj[flavor.listName],
-          flavors: [...resObj[flavor.listName].flavors, flavor.flavor],
-        },
-      };
-    });
-
-    descriptions.forEach((description) => {
-      if (resObj[description.listName].descriptions === undefined) {
-        resObj[description.listName] = {
-          effects: [...resObj[description.listName].effects],
-          flavors: resObj[description.listName].flavors,
-
-          description: [],
-        };
-      }
-      console.log(description, " this shouldn't be undefined");
-      resObj = {
-        ...resObj,
-        [description.listName]: {
-          ...resObj[description.listName],
-          description: [
-            ...resObj[description.listName].description,
-            description.userDescription,
-          ],
-        },
-      };
-    });
+    let resObj = turnIDsIntoInfo(effects, flavors, descriptions);
+    console.log("the res obj COMING IN IS ", resObj);
 
     res.status(200).json({
       message: `better data shapes, happier users. Wouldn't you say, ${user}? :)`,
@@ -124,7 +152,6 @@ router.get("/recommendations/:listName", async (req, res) => {
   try {
     let { listName } = req.params;
     let id = req.decodedJwt.subject;
-    let prefs = [];
     let exists = await Users.getUserById(id);
     console.log("DOES IT EXIST OR NOT!??!?!?!?!?!??!!? ", exists);
     if (exists.length < 1) {
@@ -133,28 +160,13 @@ router.get("/recommendations/:listName", async (req, res) => {
         error: "That's a bummer for ya",
       });
     }
-    let effects = await Users.getList(id, "effects", listName);
-    let flavors = await Users.getList(id, "flavors", listName);
-    let descriptions = await Users.getList(id, "list_descriptions", listName);
+    let prefsString = await makePrefstringFromList(id, listName);
 
-    flavors.map((flavor) => {
-      prefs.push(flavor);
-    });
-    effects.map((effect) => {
-      prefs.push(effect);
-    });
-    if (descriptions.userDescription) {
-      await Users.updatePrefs(descriptions.userDescription, "description");
-      prefs.push(descriptions.userDescription);
-    }
-    let preffies = prefs.join(" ");
-    console.log("yer preffies are ", preffies);
-    let recommendations = await getRecs(preffies);
+    let recommendations = await getRecs(prefsString);
 
     res.status(200).json({
       message: `herez yer weeeds `,
       recommendations: recommendations,
-      preffies: preffies,
     });
   } catch (err) {
     res.status(500).json({
@@ -170,15 +182,8 @@ router.post("/add-list", async (req, res) => {
     let user = req.decodedJwt.username;
     let listName = req.body.listName;
     let id = req.decodedJwt.subject;
-    //include listName in req.body please ADD THIS TO THE README
     let newPreferences = req.body;
-    // let listIDObj = await Users.getListId(listName, id);
-    // let listId = listIDObj[0].id;
-    console.log(
-      "OKAY SO HERE IS YOUR LIST NAME AND ID THAT YOU'RE SEARCHING ",
-      listName,
-      id
-    );
+
     let exists = await Users.getListId(listName, id);
     console.log("DOES IT EXIST OR NOT!??!?!?!?!?!??!!? ", exists);
     if (exists.length > 0) {
@@ -226,30 +231,20 @@ router.post("/add-list", async (req, res) => {
 
     console.log(payload.flavors);
     console.log(payload.effects);
-    let prefs = [];
 
     await Users.updatePrefs(payload.flavors, "flavor");
     await Users.updatePrefs(payload.effects, "effect");
 
-    payload.flavors.map((flavor) => {
-      prefs.push(flavor);
-    });
-    payload.effects.map((effect) => {
-      prefs.push(effect);
-    });
-    if (payload.descriptionObj.description) {
+    if (req.body.description) {
       await Users.updatePrefs(payload.descriptionObj, "description");
-      prefs.push(payload.descriptionObj.description);
     }
+    let prefsString = await makePrefstringFromList(id, listName);
 
-    let preffies = prefs.join(" ");
-    console.log("yer preffies are ", preffies);
-    let recommendations = await getRecs(preffies);
+    let recommendations = await getRecs(prefsString);
 
     res.status(200).json({
       message: `you just CREATED list: ${listName}, ${user} `,
       recommendations: recommendations,
-      preffies: preffies,
     });
   } catch (err) {
     res.status(500).json({
@@ -265,25 +260,17 @@ router.put("/update-list", async (req, res) => {
     let user = req.decodedJwt.username;
     let listName = req.body.listName;
     let id = req.decodedJwt.subject;
-    //include listName in req.body please ADD THIS TO THE README
     let newPreferences = req.body;
-    // let listIDObj = await Users.getListId(listName, id);
-    // let listId = listIDObj[0].id;
-    console.log(
-      "OKAY SO HERE IS YOUR LIST NAME AND ID THAT YOU'RE SEARCHING ",
-      listName,
-      id
-    );
+
     let exists = await Users.getListId(listName, id);
     console.log("DOES IT EXIST OR NOT!??!?!?!?!?!??!!? ", exists);
     if (exists.length < 1) {
       res.status(400).json({
-        message: "bruh. that list doesn't exist",
+        message: "bruh. a list with that name doesn't exist",
         error: "That's a bummer for ya",
       });
     }
     await Users.deleteList(listName, id);
-    console.log();
     await Users.addList(listName, id);
     let newList = await Users.getListId(listName, id);
     let allFlavors = await Users.getEffectOrFlavorIds("flavor");
@@ -322,38 +309,20 @@ router.put("/update-list", async (req, res) => {
 
     console.log(payload.flavors);
     console.log(payload.effects);
-    let prefs = [];
 
     await Users.updatePrefs(payload.flavors, "flavor");
     await Users.updatePrefs(payload.effects, "effect");
 
-    payload.flavors.map((flavor) => {
-      console.log("the flavo ", flavor);
-
-      prefs.push(flavor);
-    });
-    payload.effects.map((effect) => {
-      console.log("the effecto ", effect);
-
-      prefs.push(effect);
-    });
-    if (payload.descriptionObj.description) {
+    if (req.body.description) {
       await Users.updatePrefs(payload.descriptionObj, "description");
-      console.log(
-        "the descripto bescrippo ",
-        payload.descriptionObj.description
-      );
-      prefs.push(payload.descriptionObj.description);
     }
+    let prefsString = await makePrefstringFromList(id, listName);
 
-    let preffies = prefs.join(" ");
-
-    let recommendations = await getRecs(preffies);
+    let recommendations = await getRecs(prefsString);
 
     res.status(200).json({
       message: `you just UPDATED list: ${listName}, ${user} `,
       recommendations: recommendations,
-      preffies: prefs,
     });
   } catch (err) {
     res.status(500).json({
